86e8f44f59e6aa150eb0917d508c5a454bf67a5f
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-09-22 23:40:40 +0100

    set coffee class

---
+++ src/classes/coffee.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+import CoffeeMachine from "./coffeeMachine";
+
+class Coffee {
+    /**
+     * @param {number} coffeeBeans The amount of coffee beans in g
+     * @param {number} water The amount of water in ml
+     * @param {number} milk The amount of water in ml
+     * @param {number} price The coffee cost
+     */
+    constructor(coffeeBeans, milk, water, price) {
+        this.coffeeBeans = coffeeBeans;
+        this.milk = milk;
+        this.water = water;
+        this.price = price;
+    }
+
+    /**
+     * The amount of cups available based on the coffee type
+     * @returns {number} Available cups
+     */
+    availableCups() {
+        return CoffeeMachine.availableCups(this.coffeeBeans, this.water, this.milk);
+    }
+
+    /**
+     * Make coffee with how many cups and consume the coffee machine ingredients
+     * @param {"regular coffee" | "espresso" | "latte" | "cappuccino"} coffeeType The coffee type (e.g. espresso - latte)
+     * @param {number} cups The amount of cups to make at once (default: 1)
+     * @returns {string} Able or unable to make that amount of cups a message string is returned
+     */
+    makeCoffee(coffeeType, cups = 1) {
+        const availableCups = this.availableCups() >= cups;
+
+        if (!availableCups) return `I can't make any more ${coffeeType}`;
+
+        CoffeeMachine.consumeRessources(
+            this.coffeeBeans * cups,
+            this.water * cups,
+            this.milk * cups,
+            this.price * cups
+        );
+
+        const successMessage = `${coffeeType} is ready`;
+        return successMessage;
+    }
+};
+
+export default Coffee;

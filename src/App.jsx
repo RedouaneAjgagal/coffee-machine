d69815f1a0658346a51aeca5efe2d734b01aa188
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-09-23 02:57:35 +0100

    [FEAT] initial classes run in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+//@ts-check
+import React, { useEffect, useState } from "react";
+import { RegularCoffee, Espresso, Latte, CoffeeMachine } from "./classes";
 
 const App = () => {
 
+  // const [rerender, setIsRerender] = useState(false);
+
+  const [message, setMessage] = useState("Let's make coffee");
+
+  const regularCoffee = new RegularCoffee();
+  const espressoCoffee = new Espresso();
+  const latteCoffee = new Latte();
+
+  const makeEspresso = () => {
+    const message = espressoCoffee.maker();
+    coffeeMachineLogger();
+    setMessageHandler(message);
+  };
+
+  const makeLatte = () => {
+    const message = latteCoffee.maker();
+    coffeeMachineLogger();
+    setMessageHandler(message);
+  };
+
+  const setMessageHandler = (message) => {
+    setMessage(message);
+  }
+
+  const coffeeMachineLogger = () => {
+    console.log({
+      milk: CoffeeMachine.totalMilk,
+      water: CoffeeMachine.totalWater,
+      coffeeBeans: CoffeeMachine.totalCoffeeBeans,
+      money: CoffeeMachine.totalMoney,
+    });
+  }
+
+  coffeeMachineLogger();
+
   return (
-    <div>App</div>
+    <div className="p-4 flex gap-4">
+      <button onClick={makeEspresso} className="bg-stone-700 text-white p-3 rounded">Make Espresso</button>
+      <button onClick={makeLatte} className="bg-amber-800 text-white p-3 rounded">Make Latte</button>
+
+      <div>
+        <p>{message}</p>
+      </div>
+    </div>
   )
 }
 
